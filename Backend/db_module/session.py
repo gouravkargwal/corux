25b5d02cb57acd6fa935921cf8e7fc5b251c51d4
Gourav Kargwal <gourav.kargwal@decimalpointanalytics.com>
2024-04-28 00:26:51 +0530

    "connect_sql_gcp"

--- Backend/db_module/session.py
+++ Backend/db_module/session.py
@@ -4,21 +4,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Construct connection URL from environment variables
-database_engine = os.getenv("DATABASE_ENGINE", "mysql+pymysql")
-database_user = os.getenv("DATABASE_USER", "root")
-database_password = os.getenv("DATABASE_PASSWORD", "admin@]1234").replace("@", "%40")
-database_host = os.getenv("DATABASE_HOST", "127.0.0.1")
-database_port = os.getenv("DATABASE_PORT", "3306")
-database_name = os.getenv("DATABASE_NAME", "corux2")
-
-connection_url = f"{database_engine}://{database_user}:{database_password}@{database_host}:{database_port}/{database_name}"
+DATABASE_ENGINE = os.getenv("DATABASE_ENGINE")
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD").replace('@','%40')
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+INSTANCE_CONNECTION_NAME = os.getenv("INSTANCE_CONNECTION_NAME") 
+IP_PUBLIC = os.getenv("IP_PUBLIC")
 
 
-engine = create_engine(connection_url, pool_recycle=3600)
+DATABASE_URL = f"mysql+pymysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{IP_PUBLIC}/{DATABASE_NAME}"
+
+# Create an engine instance
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
