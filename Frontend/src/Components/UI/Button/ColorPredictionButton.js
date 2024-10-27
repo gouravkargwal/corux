28141791d1f826b52aa7b54888ac7eaec83bd9ec
Gourav Kargwal <gouravkargwalverma@gmail.com>
2024-10-27 21:57:36 +0530

    feat: Added color to bet color Button

--- Frontend/src/Components/UI/Button/ColorPredictionButton.js
+++ Frontend/src/Components/UI/Button/ColorPredictionButton.js
@@ -2,16 +2,25 @@ import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 
 export const VioletButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.button.violet,
-  backgroundColor: theme.palette.background.violet,
+  backgroundColor: "#9b59b6",
+  color: "white",
+  "&:hover": {
+    background: "#8e44ad",
+  },
 }));
 
 export const RedButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.button.red,
-  backgroundColor: theme.palette.background.red,
+  backgroundColor: "#ff4c4c",
+  color: "white",
+  "&:hover": {
+    backgroundColor: "#e04343", // Darker red on hover
+  },
 }));
 
 export const GreenButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.button.green,
-  backgroundColor: theme.palette.background.green,
+  backgroundColor: "#28a745",
+  color: "white",
+  "&:hover": {
+    backgroundColor: "#218838", // Darker green on hover
+  },
 }));
