30556ae578f07abcb98b05497aa61d7dc71d0bd0
Gourav Kargwal <gouravkargwalverma@gmail.com>
2024-12-04 15:15:57 +0530

    feat: Added more coming soon games

--- Frontend/src/Pages/HomePage.js
+++ Frontend/src/Pages/HomePage.js
@@ -3,7 +3,9 @@ import Hero from "../Components/Home/Hero";
 import { Link } from "react-router-dom";
 import GameCard from "../Components/Home/GameCard";
 import ColorPredictionImg from "../Assets/Images/wingo.webp";
-import ComingSoonImg from "../Assets/Images/comingsoon.webp";
+import AviatorImg from "../Assets/Images/aviator.webp";
+import MinesImg from "../Assets/Images/mines.webp";
+import PlinkoImg from "../Assets/Images/plinko.webp";
 import CongratsDialogue from "../Components/Home/CongratsDialog";
 import { useDispatch, useSelector } from "react-redux";
 import { closeDialog, selectDialogOpen } from "../Feature/Dialog/dialogSlice";
@@ -17,11 +19,17 @@ function GridExample() {
       <Grid container spacing={2} p={3}>
         <Grid item xs={12} sm={6} md={4} lg={3}>
           <Link to="color-prediction">
-            <GameCard title="WinGO" time="3min" img={ColorPredictionImg} />
+            <GameCard title="Color Prediction" time="3m" img={ColorPredictionImg} />
           </Link>
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <GameCard title="Monty Hall" time="30sec" img={ComingSoonImg} />
+          <GameCard title="Aviator" time="Coming Soon" img={AviatorImg} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <GameCard title="Mines" time="Coming Soon" img={MinesImg} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <GameCard title="Plinko" time="Coming Soon" img={PlinkoImg} />
         </Grid>
       </Grid>
       <CongratsDialogue
