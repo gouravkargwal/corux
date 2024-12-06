706fbcd1c3092dcc7bb2c12a3f5c6dc5577d7131
Dev <2019umt1842@mnit.ac.in>
2024-12-06 11:50:59 +0530

    #INT changes_to_random_win_admin_rd_vt_gn

--- Admin/home/admin.py
+++ Admin/home/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserAdminTable, PaymentDepositTable, PaymentWithdrawTable, UpiTable
+from .models import UserAdminTable, PaymentDepositTable, PaymentWithdrawTable, UpiTable, BetColor
 
 # Register your models here.
 admin.site.register(UserAdminTable)
 admin.site.register(PaymentDepositTable)
 admin.site.register(PaymentWithdrawTable)
 admin.site.register(UpiTable)
+admin.site.register(BetColor)
